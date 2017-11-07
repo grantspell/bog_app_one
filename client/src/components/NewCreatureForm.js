c3d76da23b6edb2b6bd009791a184d091fdb08e4
grantspell <spellgrant@gmail.com>
2017-11-07 14:11:53 -0500

    ADDED | `Creatures.js` `SingleCreature.js` `NewCreatureForm.js` and updated `Creatures.js` content with list of all creatures and new form toggle

---
+++ client/src/components/NewCreatureForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewCreatureForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default NewCreatureForm;
