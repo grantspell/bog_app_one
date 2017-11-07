d109c9a2bfff03490c82974e1e90f95ed8ddaa1c
grantspell <spellgrant@gmail.com>
2017-11-07 10:48:28 -0500

    Parts I && II Complete | Index, Create, and Show routes created and some seed data

---
+++ app/controllers/api/creatures_controller.rb
@@ -0,0 +1,31 @@
+class Api::CreaturesController < ApplicationController
+
+    def index
+        @creatures = Creature.all
+
+        render json: @creatures
+    end
+
+    def create
+        @creature = Creature.new(creature_params)
+
+        if @creature.save
+            render json: @creature
+        end
+    end
+
+    def show
+        creature_id = params[:id]
+
+        @creature = Creature.find_by_id(creature_id)
+
+        render json: @creature
+    end
+
+    private
+
+    def creature_params
+        params.require(:creature).permit(:name, :description)
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+      get "/creatures", to: "creatures#index", as: "creatures"
+      post "/creatures", to: "creatures#create"
+      get "/creatures/:id", to: "cratures#show", as "creature"
+  end
 end
