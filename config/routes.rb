1018d510eabf44181f78869e81204e804dcf96e3
grantspell <spellgrant@gmail.com>
2017-11-07 10:54:45 -0500

    Fixed Show Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
       get "/creatures", to: "creatures#index", as: "creatures"
       post "/creatures", to: "creatures#create"
-      get "/creatures/:id", to: "cratures#show", as "creature"
+      get "/creatures/:id", to: "creatures#show", as: "creature"
   end
 end
